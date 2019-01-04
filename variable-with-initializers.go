cebbd0ca89543d2c6b682629d3f2bdb5d4a0e934
sakenism <sakenism@gmail.com>
2019-01-04 15:15:15 -0500

    ds

---
+++ variable-with-initializers.go
@@ -0,0 +1,11 @@
+package main
+import (
+       "fmt"
+)
+
+var i, j int = 1, 2
+
+func main() {
+     var c, python, java, q = true, false, "no!", "int"
+     fmt.Println(i, j, c, python, java, q)
+}
