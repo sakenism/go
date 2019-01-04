427641ca25129c83d9c3ab87df31c190c3a3727a
sakenism <sakenism@gmail.com>
2019-01-04 15:39:46 -0500

    done 1st module

---
+++ constants.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14159265358
+
+func main() {
+     const World = "sakenism"
+     const Truth = true
+     fmt.Println("Hello", World)
+     fmt.Println("Happy", Pi, "Day")
+     fmt.Println("Go rules?", Truth)
+}
---
+++ type-interference.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+     v := 42.42 + 1i
+     fmt.Printf("v is of type %T\n", v)
+}
