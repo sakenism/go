14e727165f07ff74b63404b00606c366380e582f
sakenism <sakenism@gmail.com>
2019-01-06 05:16:28 +0600

    D

---
+++ slices-pointers.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	names := [4]string {
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
+	a := names[0 : 2]
+	b := names[1 : 3]
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
+}
